8a201d2f7becb6afbced4e3593a915a3a053be76
Fabian Laske <104904431+doctorGlocktopus@users.noreply.github.com>
2022-11-18 14:37:49 +0100

    MailChatChronJobReminder

--- mailService.js
+++ mailService.js
@@ -1,45 +1,64 @@
-const nodemailer = require('nodemailer');
 const cron = require('node-cron');
 const Task = require('./models/task');
+const Mailjet = require('node-mailjet');
 require('dotenv').config();
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-    },
-});
-
-// cron.schedule('59 23 * * 3', function () { // immer 59minuten, 23stunden, 0Tage, 0WOchen, dritter WOchentag
-cron.schedule('* * * * * *', async function () {
+// cron.schedule('* * * * * * ', async function () { // jede Sekunde
+cron.schedule('59 23 * * 3', async function () { // immer 59minuten, 23stunden, 0Tage, 0WOchen, dritter WOchentag
     console.log('---------------------');
     console.log('Running Cron Job');
     const tasks = await Task.find({done: false}).populate("user_id", {password: false});
     if (tasks != []) {
         tasks.forEach(task => {
             if (task.done === false) {
-                let mailOptions = {
-                    from: 'reactLearn@anche.no', // Sender address
-                    to: task.user_id.email, // List of recipients
-                    subject: 'Warnung!', // Subject line
-                    html:
-                    `
-                    Hey!, die Aufgabe Nr ${task._id} mit dem Titel ${task.title} wurde immer noch nicht erledigt
-                    <a href = "http://localhost:3000/tasks/${task._id}">
-                        Link zur Aufgabe
-                    </a>
-                    `,
-                };
+                const mailjet = Mailjet.apiConnect(process.env.PUBLICMAIL,
+                    process.env.PRIVATEMAIL,
+                    {
+                        config: {},
+                        options: {},
+                    });
+                const request = mailjet
+                    .post('send', {version: 'v3.1'})
+                    .request({
+                        "Messages": [
+                            {
+                                "From": {
+                                    "Email": "laskoferrari@gmail.com",
+                                    "Name": "Fabian",
+                                },
+                                "To": [
+                                    {
+                                        "Email": `${task.user_id.email}`,
+                                        "Name": `${task.usernamse}`,
+                                    },
+                                ],
+                                "Subject": "Aufgabe noch nicht erledigt",
+                                "TextPart":
+                                `
+                                Hey!, die Aufgabe Nr ${task._id} mit dem Titel ${task.title} wurde immer noch nicht erledigt
+                                    <a href = "http://localhost:3000/tasks/${task._id}">
+                                        Link zur Aufgabe
+                                    </a>
+                                `,
+                                "HTMLPart":
+                                `
+                                Hey!, die Aufgabe Nr ${task._id} mit dem Titel ${task.title} wurde immer noch nicht erledigt
+                                    <a href = "http://localhost:3000/tasks/${task._id}">
+                                        Link zur Aufgabe
+                                    </a>
+                                `,
+                                "CustomID": "AppGettingStartedTest",
+                            },
+                        ],
+                    });
 
-                transport.sendMail(mailOptions, function (err, info) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(info);
-                    }
-                });
+                request
+                    .then((result) => {
+                        console.log(result.body);
+                    })
+                    .catch((err) => {
+                        console.log(err.statusCode);
+                    });
             }
         });
     }
