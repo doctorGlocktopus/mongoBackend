c908d198d81cdb952a3398613b1f728bf771deee
Fabian Laske <104904431+doctorGlocktopus@users.noreply.github.com>
2022-12-03 12:01:56 +0100

    smal fix

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const cors = require('cors');
 
 app.use(cors());
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+});
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', (error) => console.log('Connection to Database'));
